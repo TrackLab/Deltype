63f160977efb36fa90acadd1023e8ef01e3b2266
Saren Dev <54124554+SarenDev@users.noreply.github.com>
2020-03-02 00:45:30 +0300

    Added custom directory support and fixes
    - Added support for passing in a custom directory
    - Touched up unused and redundant
    - Fixed bug, causing uncontrolled data destruction if no extension is given
    - Added several precautions

--- deltype.py
+++ deltype.py
@@ -2,25 +2,30 @@
 # Requirements - When using the delfiletype.exe, None.
 #                When using this source code, send2trash                
 
-from os import listdir, remove
+import os
+from sys import argv
 from send2trash import send2trash
 
-def deletefile(filetype=None):
-    for f in listdir():
+def deletefile(filetype):
+    for f in os.listdir():
         if f.endswith(filetype):
-            send2trash(f)       # replace with remove(f) to use python based libraries only
-            print('Deleted', f)
-    print('Successfully deleted all files with the filetype', filetype)
-    
-try:
-    filetype = input('What filetype should be deleted? ').lower()
-    if filetype.startswith('.'):
-        filetype = filetype[1:].lower()
-    if filetype == 'py':
-        inp = input('That will also delete this script file. Are you sure? y/n \n').lower()
-        if inp == 'y':
-            deletefile(filetype)
-    else:
-        deletefile(filetype)
+            send2trash(f)       # Replace with os.remove(f) to use python-only libraries but lose undo-ability
+            print('\nDeleted', f)
+    print('\nSuccessfully everything with the filetype', filetype)
+
+try:    
+    os.chdir(argv[1])
 except:
-    raise ValueError('No filetype to delete has been provided.')
+    if input('\nRun in the directory: '+os.getcwd()+'? y/n\n').lower() == 'y':
+        pass
+    else:
+        print('Please specify a directory when running the script...\n')
+        quit()
+filetype = input('\nWhat filetype should be deleted?\n').lower()
+if filetype == '':
+    if input('\nA blank filetype will WIPE YOUR DIRECTORY! Are you sure you want to continue? y/n\n').lower() == 'y':
+        deletefile(filetype)
+    quit()
+if filetype.startswith('.'):
+    filetype = filetype[1:].lower()
+deletefile(filetype)
